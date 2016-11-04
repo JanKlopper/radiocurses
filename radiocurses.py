56466a4eece0e137eb8a043df3439cce3f49e032
Jan Klopper <janklopper@innerheight.com>
2016-11-04 11:26:28 +0100

    Add Page up / Page down key press support Hit q for quiting the program Remove lirc / joystick errors in mplayer output by disabling those features

--- radiocurses.py
+++ radiocurses.py
@@ -3,6 +3,7 @@
 __author__ = 'Jan Klopper (jan@underdark.nl)'
 __version__ = 0.1
 
+import curses
 from cursesmenu import *
 from cursesmenu.items import *
 
@@ -16,9 +17,37 @@ DICHANNELS = ['http://www.di.fm/webplayer3/config',
               'http://www.jazzradio.com/webplayer3/config',
               'http://www.radiotunes.com/webplayer3/config']
 DIURLPREMIUM = 'http://prem2.di.fm:80/%s_hi?%s'
-PLAYER = 'mplayer %s'
+PLAYER = 'mplayer -nolirc -nojoystick -quiet %s'
 CODE = '' # fill this with your premium code
 MAXCACHEAGE = (60*60*24*7) # in seconds
+PAGEOFFSET = 15
+
+class MyCursesMenu(CursesMenu):
+  def process_user_input(self):
+    """
+    Gets the next single character and decides what to do with it
+    """
+    user_input = self.get_input()
+
+    go_to_max = ord("9") if len(self.items) >= 9 else ord(str(len(self.items)))
+
+    if ord('1') <= user_input <= go_to_max:
+      self.go_to(user_input - ord('0') - 1)
+    elif user_input == curses.KEY_PPAGE:
+      for i in xrange(0, PAGEOFFSET):    
+        self.go_up()
+    elif user_input == curses.KEY_NPAGE:
+      for i in xrange(0, PAGEOFFSET):
+        self.go_down()
+    elif user_input == curses.KEY_DOWN:
+      self.go_down()
+    elif user_input == curses.KEY_UP:
+      self.go_up()
+    elif user_input == ord("\n"):
+      self.select()
+    elif user_input == ord("q"):
+      self.exit()
+    return user_input
 
 class radiocurses(object):
 
@@ -27,9 +56,8 @@ class radiocurses(object):
     self.dichannels = []
     self.ReadDiChannels()
 
-    menu = CursesMenu("Radio Curses by Frack.nl", "Select a channel:")
+    menu = MyCursesMenu("Radio Curses by Frack.nl", "Select a channel:")
     menu.append_item(FunctionItem("Refresh DI.fm cache", self.fetchDiFM))
-
 
     code = (self.options.premium if self.options.premium else CODE)
     for channel in self.dichannels:
@@ -66,4 +94,4 @@ if __name__ == '__main__':
   parser = optparse.OptionParser()
   parser.add_option('-p', action="store", dest="premium", default=None)
   options, remainder = parser.parse_args()
-  radio = radiocurses(options)
+  radio = radiocurses(options)  
